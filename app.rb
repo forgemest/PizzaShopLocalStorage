5b716f4d87ef1d31b67fb31cd1974d9cc611b4b2
forgemes <forgemest@gmail.com>
2023-07-12 10:51:16 +0200

    add Local Storage

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+get '/' do
+   erb :index
+end
+
+get '/about' do 
+   erb :about
+end
---
+++ public/app.js
@@ -0,0 +1,11 @@
+function something()
+{
+	var x = window.localStorage.getItem('aaa');
+
+	window.localStorage.setItem('aaa', 555);
+
+	alert(x);
+	
+}
+
+
